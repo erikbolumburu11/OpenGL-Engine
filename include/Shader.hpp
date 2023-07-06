51650de4759ecf9ef9e3de0830f1ce930b372fa9
Erik Bolumburu <108585396+erikbolumburu11@users.noreply.github.com>
2023-07-06 01:39:12 +0100

    Fixed Styling Of Function Titles

--- include/Shader.hpp
+++ include/Shader.hpp
@@ -14,15 +14,15 @@ struct Shader {
 		glUseProgram(ID);
 	}
 
-	void setBool(const std::string& name, bool value) const
+	void SetBool(const std::string& name, bool value) const
 	{
 		glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 	}
-	void setInt(const std::string& name, int value) const
+	void SetInt(const std::string& name, int value) const
 	{
 		glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
 	}
-	void setFloat(const std::string& name, float value) const
+	void SetFloat(const std::string& name, float value) const
 	{
 		glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 	}
